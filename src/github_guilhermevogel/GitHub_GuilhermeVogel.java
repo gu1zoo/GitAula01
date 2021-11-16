93271add6f92bfa2f9529cf9578db9f2037a9b2c
unknown <IFSC@CDR-691780.cacador.ifsc.edu.br>
2021-11-16 15:32:02 -0300

    Impressão com Scanner

---
+++ src/github_guilhermevogel/GitHub_GuilhermeVogel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github_guilhermevogel;
+import java.util.Scanner;
+/**
+ *
+ * @author IFSC
+ */
+public class GitHub_GuilhermeVogel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome:");
+        String nome =  scanner.nextLine();
+        
+        System.out.println(nome +" Bem vindo ao gitHub");
+        // TODO code application logic here
+    }
+    
+}
